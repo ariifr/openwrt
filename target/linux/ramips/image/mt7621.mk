1c5311079c29cf1f6ac63195c108a619499cb814
ariifr <reev.rm+1@gmail.com>
2025-11-20 23:34:27 +0700

    Add support for ZTE E8820V2 device
    Signed-off-by: ariifr <reev.rm+1@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1677,3 +1677,14 @@ define Device/zyxel_wap6805
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += zyxel_wap6805
+
+define Device/zte_e8820v2
+  $(Device/dsa-migration)
+  $(Device/uimage-lzma-loader)
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := ZTE
+  DEVICE_MODEL := E8820V2
+  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb2 \
+          kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += zte_e8820v2
